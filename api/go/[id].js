3ae1749b491724be65b42798dcf0a98fb069df3f
rcrept-spec <rcrept@gmail.com>
2026-06-14 16:45:36 -0300

    test tracking

--- api/go/[id].js
+++ api/go/[id].js
@@ -1,7 +1,29 @@
 export default async function handler(req, res) {
   const { id } = req.query;
 
-  console.log("HIT:", id);
+  try {
+    const response = await fetch(
+      `${process.env.SUPABASE_URL}/rest/v1/clicks`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "apikey": process.env.SUPABASE_KEY,
+          "Authorization": `Bearer ${process.env.SUPABASE_KEY}`,
+          "Prefer": "return=minimal"
+        },
+        body: JSON.stringify({
+          link_id: id
+        })
+      }
+    );
+
+    console.log("Supabase status:", response.status);
+    console.log("Supabase text:", await response.text());
+
+  } catch (err) {
+    console.error("Supabase error:", err);
+  }
 
   res.writeHead(302, {
     Location: "/?link=" + id

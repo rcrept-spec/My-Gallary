ca9330ef38b944a74392849b1cee970d09748ffd
rcrept-spec <rcrept@gmail.com>
2026-06-14 16:24:36 -0300

    test tracking

--- api/go/[id].js
+++ api/go/[id].js
@@ -1,10 +1,8 @@
 export default function handler(req, res) {
   const { id } = req.query;
 
-  console.log("Tracking link opened:", id);
-
   res.writeHead(302, {
-    Location: "/"
+    Location: "/?link=" + encodeURIComponent(id)
   });
 
   res.end();
